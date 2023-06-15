5481d256407ff01d3fdd72d34e665833f7497f66
soyoung125 <sypark041754@gmail.com>
2023-06-15 16:34:32 +0900

    feat: Assignment5 - /todo 경로 접속시 투두 리스트 목록 그리기 - 완료 여부를 나타내는 checkbox 추가 - <li>태그로 todo 감싸기

--- src/container/todo/TodoContainer.tsx
+++ src/container/todo/TodoContainer.tsx
@@ -1,18 +1,64 @@
-import React, { useEffect } from 'react';
+import { Checkbox, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import EditIcon from '@mui/icons-material/Edit';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+interface TodoInterface {
+  id: number,
+  todo: string,
+  isCompleted: boolean,
+  userId: number,
+}
 
 const TodoContainer = () => {
   const navigate = useNavigate();
+  const data = localStorage.getItem('data');
+  const [todos, setTodos] = useState<TodoInterface[]>([]);
   
   useEffect(() => {
-    if(!localStorage.getItem('data')) {
+    if(!data) {
       console.log(11)
       navigate('/signin', {replace: true})
+    } 
+    else {
+      setTodos(getData(JSON.parse(data)));
     }
   }, [])
 
+  
+  const getData = (data: { access_token: string; }): TodoInterface[] => {
+    axios.get('https://www.pre-onboarding-selection-task.shop/todos', {
+        headers: {
+          Authorization: `Bearer ${data.access_token}`
+        }
+      }).then((res) => { return res.data; } )
+      .catch((err) => { console.log(err) })
+    return [];
+  }
+
+
+
   return (
-    <>todo</>
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {todos.map((todo) => (
+        <ListItem
+          key={todo.id}
+          disablePadding
+        >
+          <ListItemIcon>
+            <Checkbox
+              edge="start"
+              checked={todo.isCompleted}
+              tabIndex={-1}
+              disableRipple
+            />
+          </ListItemIcon>
+          <ListItemText primary={todo.todo} />
+        </ListItem>
+      ))}
+    </List>
   );
 }
 
