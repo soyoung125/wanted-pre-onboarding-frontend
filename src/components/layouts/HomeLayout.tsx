9b05684f7e183ec6df8d24d2545e7e6dcba5621c
soyoung125 <sypark041754@gmail.com>
2023-06-15 22:45:10 +0900

    feat: 상단바 추가 - Todo 클릭시 홈으로 이동 - todolist 클릭시 /todo로 이동 - 로그인/로그아웃 버튼 추가

--- src/components/layouts/HomeLayout.tsx
+++ src/components/layouts/HomeLayout.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import { Container } from "@mui/material";
+import { AppBar, Box, Button, Container, IconButton, Toolbar, Typography } from "@mui/material";
 import { useRef } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const HomeLayout = () => {
   const ref = useRef(null);
+  const navigate = useNavigate();
+  const auth = localStorage.getItem('data');
 
   return (
-    <Container  component="main" ref={ref}>
-      <Outlet />
-    </Container>
+    <>
+      <AppBar position='static' component="nav">
+        <Toolbar variant="dense">
+          <Typography onClick={() => navigate('/')} variant="h6" component="div" sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+            Todos
+          </Typography>
+          {auth
+            ? <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              <Button sx={{ color: '#fff' }} onClick={() => navigate('/todo')}>
+                todo list
+              </Button>
+              <Button sx={{ color: '#fff' }} onClick={() => {localStorage.removeItem('data'); navigate('/')}}>로그아웃</Button>
+            </Box>
+            : <Button sx={{ color: '#fff' }} onClick={() => navigate('/signin')}>로그인</Button>
+          }
+
+        </Toolbar>
+      </AppBar>
+      <Container component="main" ref={ref} sx={{ mt: 1 }}>
+        <Outlet />
+      </Container>
+    </>
   );
 }
 
