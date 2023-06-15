11e91b33db02f1374ce308e4911ca5ce9cf9576a
soyoung125 <sypark041754@gmail.com>
2023-06-15 17:41:38 +0900

    feat: Assignment 6 - todo 입력칸, 버튼 추가 - 버튼 클릭시 입력 내용 todo로 추가 - todo 추가 후 새로고침해도 todo 목록 보임 - getData에서 응답 body를 반환하기 전 빈배열을 반환하는 문제가 있어 async/await으로 수정

--- src/container/todo/TodoContainer.tsx
+++ src/container/todo/TodoContainer.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { Button, Checkbox, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, TextField } from '@mui/material';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditIcon from '@mui/icons-material/Edit';
 import axios from 'axios';
@@ -14,51 +14,97 @@ interface TodoInterface {
 
 const TodoContainer = () => {
   const navigate = useNavigate();
-  const data = localStorage.getItem('data');
+  const data = JSON.parse(localStorage.getItem('data') || '');
   const [todos, setTodos] = useState<TodoInterface[]>([]);
-  
+  const [todoInput, setTodoInput] = useState('');
+
   useEffect(() => {
-    if(!data) {
-      console.log(11)
-      navigate('/signin', {replace: true})
-    } 
-    else {
-      setTodos(getData(JSON.parse(data)));
-    }
-  }, [])
+    const fetchData = async () => {
+      if (!data) {
+        navigate('/signin', { replace: true });
+      } else {
+        try {
+          const todosData = await getData();
+          console.log(todosData);
+          setTodos(todosData);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
 
   
-  const getData = (data: { access_token: string; }): TodoInterface[] => {
-    axios.get('https://www.pre-onboarding-selection-task.shop/todos', {
+  const getData = async (): Promise<TodoInterface[]> => {
+    try {
+      const response = await axios.get('https://www.pre-onboarding-selection-task.shop/todos', {
         headers: {
-          Authorization: `Bearer ${data.access_token}`
+          'Authorization': `Bearer ${data.access_token}`
         }
-      }).then((res) => { return res.data; } )
-      .catch((err) => { console.log(err) })
-    return [];
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+  
+
+  const createTodo = async () => {
+  if (data) {
+    try {
+      const response = await axios.post('https://www.pre-onboarding-selection-task.shop/todos', {todo: todoInput}, {
+        headers: {
+          'Authorization': `Bearer ${data.access_token}`,
+          'Content-Type': 'application/json'
+        }
+      });
+      const newData = response.data;
+      setTodos(todos.concat(newData));
+      setTodoInput('');
+    } catch (error) {
+      console.log(error);
+    }
   }
-
-
+};
 
   return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {todos.map((todo) => (
-        <ListItem
-          key={todo.id}
-          disablePadding
-        >
-          <ListItemIcon>
-            <Checkbox
-              edge="start"
-              checked={todo.isCompleted}
-              tabIndex={-1}
-              disableRipple
-            />
-          </ListItemIcon>
-          <ListItemText primary={todo.todo} />
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <TextField
+        id="todo-input"
+        placeholder='할 일을 입력하세요.'
+        variant="standard"
+        inputProps={{  'data-testid': "new-todo-input"  }}
+        value={todoInput}
+        onChange={(e) => setTodoInput(e.target.value)}
+      />
+      <Button  data-testid="new-todo-input" onClick={createTodo}>추가하기</Button>
+
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        {todos.map((todo) => (
+          <ListItem
+            key={todo.id}
+            disablePadding
+          >
+            <ListItemIcon>
+              <Checkbox
+                edge="start"
+                checked={todo.isCompleted}
+                tabIndex={-1}
+                disableRipple
+              />
+            </ListItemIcon>
+            <ListItemText primary={todo.todo} />
+
+            <IconButton><DeleteOutlineIcon /></IconButton>
+            <IconButton><EditIcon /></IconButton>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 }
 
